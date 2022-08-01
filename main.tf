257de65a963da8c1c13b83d3e86038bb683a2932
gauravgitdir <57554689+gauravgitdir@users.noreply.github.com>
2022-08-01 12:54:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -140,7 +140,7 @@ resource "aws_cloudwatch_metric_alarm" "sa_example_cpu_utilization" {
   comparison_operator = "GreaterThanOrEqualToThreshold"
 }
 
-resource "aws_db_event_subscription" "sa_example_DBEventSubscription" {
+/* resource "aws_db_event_subscription" "sa_example_DBEventSubscription" {
   count = local.instance_count
   enabled          = true
   event_categories = ["configuration change", "failure","deletion","availability","backup","failover","maintenance","notification","read replica","recovery","low storage"]
@@ -156,4 +156,167 @@ resource "aws_db_event_subscription" "sa_example_DBEventSubscription" {
 
   depends_on = [aws_sns_topic.example-sns-topic]
 }
+*/
 
+/*
+  
+# CPU_Utilization Alarm
+resource "aws_cloudwatch_metric_alarm" "sa_example_cpu_utilization" {
+  count = local.instance_count
+  alarm_name          = "rds-${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}-CPU"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  unit                = "Percent"
+  period              = 300
+  evaluation_periods  = 2
+  statistic           = "Average"
+  threshold           = 80
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  alarm_description = "RDS CPU Utilization for RDS aurora cluster ${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}"
+  alarm_actions     = [aws_sns_topic.example-sns-topic.arn]
+  insufficient_data_actions = [aws_sns_topic.example-sns-topic.arn]
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.example-postgresql-cluster.cluster_identifier
+  }
+  tags = {
+    Name = "serviceadvisor_example_cpu_utilization"
+    component = var.component
+  }
+}
+# Freeable Memory Alarm
+resource "aws_cloudwatch_metric_alarm" "sa_example_freeable_memory" {
+  count = local.instance_count
+  alarm_name          = "rds-${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}-FreeableMemory"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 3
+  metric_name         = "FreeableMemory"
+  namespace           = "AWS/RDS"
+  unit                = "Bytes"
+  period              = 300
+  statistic           = "Average"
+  threshold           = var.freeable_threshold
+    alarm_description = "RDS freeable memory for RDS aurora cluster ${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}"
+  alarm_actions     = [aws_sns_topic.example-sns-topic.arn]
+  insufficient_data_actions        = [aws_sns_topic.example-sns-topic.arn]
+
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.example-postgresql-cluster.cluster_identifier
+  }
+  tags = {
+    Name = "serviceadvisor_example_freeable_memory"
+    component = var.component
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "sa_example_trans_Wrap" {
+  count = local.instance_count
+  alarm_name          = "rds-${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}-transwap"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 3
+  metric_name         = "MaximumUsedTransactionIDs"
+  unit                = "Count"
+  namespace           = "AWS/RDS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = var.trans_wrap_threshold
+  alarm_description = "RDS swap usage for RDS aurora cluster ${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}"
+  alarm_actions     = [aws_sns_topic.example-sns-topic.arn]
+  insufficient_data_actions    = [aws_sns_topic.example-sns-topic.arn]
+
+  dimensions = {
+    DBClusterIdentifier = aws_rds_cluster.example-postgresql-cluster.cluster_identifier
+  }
+  tags = {
+    Name = "serviceadvisor_example_trans_Wrap"
+    component = var.component
+  }
+}
+#--------------------------------------------
+# LogGroup
+#--------------------------------------------
+## Keep LogGroup name in patteren /aws/rds/example-cluster/<cluster_name>/<item>,
+## even if we specify different log group it will create default LogGroup additionally in above pattern and,
+## logs will be redirected to default LogGroup
+resource "aws_cloudwatch_log_group" "example-postgresql-LogGroup-postgresql" {
+  name = var.log_group_name
+  retention_in_days = 7
+
+  tags = {
+    Name = "diagnostics-example-postgresql-LogGroup"
+    component = var.component
+  }
+}
+# Audit filters and alarms
+resource "aws_cloudwatch_log_metric_filter" "example_auth_audit_filter" {
+  name           = var.audit_auth_filter_name
+  pattern        = "authentication failed for user"
+  log_group_name = aws_cloudwatch_log_group.example-postgresql-LogGroup-postgresql.name
+  metric_transformation {
+    name      = "FailedAuthCount"
+    namespace = var.audit_auth_namespace
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "sa_example_auth_audit_alarm" {
+  count = local.instance_count
+  alarm_name          = "rds-${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}-AuthAuditAlarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 12
+  metric_name         = "FailedAuthCount"
+  #unit                = "Count"
+  namespace           = var.audit_auth_namespace
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 5
+  treat_missing_data = "notBreaching"
+  datapoints_to_alarm = 1
+  alarm_description = "RDS swap usage for RDS aurora cluster ${aws_rds_cluster_instance.example-postgresql-instance.*.id[count.index]}"
+  alarm_actions     = [aws_sns_topic.example-sns-topic.arn]
+  insufficient_data_actions    = [aws_sns_topic.example-sns-topic.arn]
+
+  depends_on = [aws_cloudwatch_log_metric_filter.example_auth_audit_filter]
+  
+  tags = {
+    Name = "serviceadvisor_example_auth_audit_alarm"
+    component = var.component
+  }
+
+}
+*/
+/*
+  resource "aws_db_parameter_group" "examplepostgresql-instance-param-grp" {
+  name                          = var.db_instance_parameter_grp_name_instance
+  family                        = var.db_family
+  description                   = "Parameter group for Amazon Aurora Postgres DB"
+  parameter {
+    name = "log_connections"
+    value = "1"
+  }
+  parameter {
+    name = "log_disconnections"
+    value = "1"
+  }
+  parameter {
+    name = "log_statement"
+    value = "none"
+  }
+  parameter {
+    name = "log_duration"
+    value = "0"
+  }
+  parameter {
+    name = "log_min_duration_statement"
+    value = "10"
+  }
+  parameter {
+    name = "log_hostname"
+    value = "0"
+  }
+
+  tags = {
+    Name = var.db_instance_parameter_grp_name_instance
+    component = var.component
+  }
+}
+*/
